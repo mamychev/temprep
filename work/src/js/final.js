081e67f429bb67865c8b35695859d088ce417953
mamychev <mamychev.v@gmail.com>
2020-06-16 15:59:41 +0700

    delete readme file

--- work/src/js/final.js
+++ work/src/js/final.js
@@ -1,20 +1,35 @@
 // jquery
 $( function(){
 
-    black_box = $("#black-box");
-    menu = $("#menu");
-    menu_icon = $(".menu-icon-adapt");
-    menu_items = $(".menu-item");
-    slash = $(".slash")
-    menu_icon.on("click",menuExpandF);
+    let arrays = $(".work_examples__array");
+    let left_array = $(".work_examples__array-left");
+    let right_array = $(".work_examples__array-right");
+    let figures = $(".work_examples__figures figure");
+    let number_of_figures = figures.length;
+    arrays.on("click",ArrayF);
 
-    function menuExpandF() {
-        black_box.toggle();
-        menu.toggleClass("menu-adapt");
-        menu_items.toggleClass("menu-item-adapt");
-        slash.toggleClass("slash-adapt");
+    let step = 390;
+    let position = 1;
+    
+    function ArrayF(event) {
+        let left = $(event.target).is(".work_examples__array-left");
+        let left_enable = (position<7);
+        let right_enable = (position>1);
+        let condition = left ? left_enable : right_enable;
+        let translate_value = left ? -step*position : -step*(position-2);
+        if(condition) {
+            figures.css("transform", `translateX(${translate_value}px)`);
+            left ? position++ : position--;
+        }
+        position<7 ? left_array.css("opacity","1") : left_array.css("opacity","0.3");
+        position>1 ? right_array.css("opacity","1") : right_array.css("opacity","0.3");
     }
 
+
+//    black_box.toggle();
+    //    menu.toggleClass("menu-adapt");
+    //    menu_items.toggleClass("menu-item-adapt");
+      //  slash.toggleClass("slash-adapt");
 
 
 });
